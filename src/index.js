72e1074e4ea551e3bf532e3aa58a031ab2573ab1
Abdul Rahman Yahya Zakaria <128055968+abdelrahman-yahia-zakaria@users.noreply.github.com>
2023-10-29 17:48:29 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// NAVIGATION
+import Nav from "./App";
+// PAGES
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Contact from "./pages/ContactPage";
+
+import "./index.css";
+
+const App = () => (
+  <div>
+    <Nav />
+    <div className="container">
+      <Route exact={true} path="/" component={Home} />
+      <Route exaxt path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+    </div>
+  </div>
+);
+
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
